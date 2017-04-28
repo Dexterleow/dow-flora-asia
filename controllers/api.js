136dc44a9420c26456c8f7392a593ffab96e4315
dexterleow <gdleow@gmail.com>
2017-04-29 00:35:15 +0800

    Loaded Google sheets API

--- controllers/api.js
+++ controllers/api.js
@@ -7,4 +7,172 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+// Home page
+// router.get('/home/:landing', (req, res) => {
+router.get('/home', (req, res) => {
+  console.log('entered /home route');
+  // pass callback function into api call to return data
+  retrieveSheets (function (apiLandingData) {
+    res.json({'apiLandingResult': apiLandingData});
+  }, req.params.landing);
+});
+
+// GOOGLE SHEETS API CALL
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+var sheetsArray;
+
+// fnOne, fnTwo and fnThree are callbacks
+// Load client secrets from a local file.
+// fnOne, fnTwo and fnThree are callbacks
+// Load client secrets from a local file.
+function retrieveSheets(fnOne, urlRoute) {
+
+  switch (urlRoute) {
+    case 'logo':
+      sheetsArray = ['logo!A2:C'];
+      break;
+    // case 'cover-photo':
+    //   sheetsArray = ['cover-photo!A2:C'];
+    //   break;
+    // case 'portfolio':
+    //   sheetsArray = ['category!A2:D'];
+    //   break;
+    // case 'portfolioactualday':
+    //   sheetsArray = ['actual-day!A2:C'];
+    //   break;
+    // case 'portfoliolifestyle':
+    //   sheetsArray = ['lifestyle!A2:C'];
+    //   break;
+    // case 'portfoliooverseas':
+    //   sheetsArray = ['overseas!A2:C'];
+    //   break;
+    // case 'portfolioprewedding':
+    //   sheetsArray = ['pre-wedding!A2:C'];
+    //   break;
+  }
+
+  fs.readFile('./secret/client_secret.json', function processClientSecrets(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // Google Sheets API.
+    authorize(JSON.parse(content), listMajors, fnOne);
+  });
+}
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback, fnTwo) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client, fnTwo);
+    }
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+ function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+/**
+ * Print the names and majors of students in a sample spreadsheet:
+ * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ *https://docs.google.com/spreadsheets/d/1vSK20rCOrGkkhRv5ZrqIQuJDeAbemeYn4K1HvXH-Ec0/edit#gid=0
+ */
+function listMajors(auth, fnThree) {
+  var sheets = google.sheets('v4');
+  // sheets.spreadsheets.values.get({ // this gets single sheet
+  sheets.spreadsheets.values.batchGet({ // this gets multiple sheets
+    auth: auth,
+    spreadsheetId: '1vSK20rCOrGkkhRv5ZrqIQuJDeAbemeYn4K1HvXH-Ec0',
+    ranges: sheetsArray, // if get multiple sheets need to use 'ranges'. if single sheet use 'range'
+    // what's this huh?
+    valueRenderOption: 'FORMATTED_VALUE'
+
+  }, function(err, response) {
+    console.log('see here for response', response);
+    console.log('batch get success');
+
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var googleSheetsHomeData = [];
+    for (var i = 0; i < response.valueRanges.length; i++) {
+      googleSheetsHomeData.push(response.valueRanges[i].values);
+    }
+
+    console.log('logging googleSheetsHomeData', googleSheetsHomeData);
+    fnThree(googleSheetsHomeData);
+  });
+}
+
 module.exports = router;
