767bb5cc6dc86ca3122d4954977dabaf1312552c
dexterleow <gdleow@gmail.com>
2017-05-20 01:33:50 +0800

    removed bootstrap carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GoogleSheetsService } from '../shared/services/google-sheets.service';
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ coverPhotoUrl: string;
+  apiCoverPhotoEndPoint: string;
+  apiCoverPhotoResult: Array<string>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private googleSheetsService: GoogleSheetsService) {
   }
 
+  ngOnInit() {
+    this.apiCoverPhotoEndPoint = '/home/cover-photo';
+    this.apiCoverPhotoResult = [];
+
+    this.getImagesFromSheets(this.apiCoverPhotoEndPoint);
+  }
+  getImagesFromSheets(sheetName) {
+    this.googleSheetsService.getImages(sheetName)
+    .subscribe(dataFromAPI => {
+      this.apiCoverPhotoResult = dataFromAPI.apiLandingResult[0];
+      this.coverPhotoUrl = this.apiCoverPhotoResult[0][3];
+
+      // need to run loop here if decide to set an interval and change cover photo every few seconds
+
+      // localStorage.setItem('sixtySecondsCategoryPhotos', JSON.stringify(this.apiCoverPhotoResult));
+
+      console.log(this.coverPhotoUrl);
+      console.log('cover photo api call success');
+      console.log(this.apiCoverPhotoResult);
+    });
+  }
 }
