5fad87ea524c03fe6988db692b3d88e6d2ccaf34
dexterleow <gdleow@gmail.com>
2017-05-21 23:20:42 +0800

    Length error in Lingga Island's photos array

--- src/app/lingga-island/lingga-island.component.ts
+++ src/app/lingga-island/lingga-island.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GoogleSheetsService } from '../shared/services/google-sheets.service';
+
 @Component({
   selector: 'app-lingga-island',
   templateUrl: './lingga-island.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinggaIslandComponent implements OnInit {
 
-  constructor() { }
+  linggaIslandPhotoUrl: string;
+  apilinggaIslandPhotoEndPoint: string;
+  apilinggaIslandPhotoResult: Array<string>;
+
+  constructor(private googleSheetsService: GoogleSheetsService) {
+  }
 
   ngOnInit() {
+    this.apilinggaIslandPhotoEndPoint = '/home/lingga';
+    this.apilinggaIslandPhotoResult = [];
+
+    this.getImagesFromSheets(this.apilinggaIslandPhotoEndPoint);
+  }
+  getImagesFromSheets(sheetName) {
+    this.googleSheetsService.getImages(sheetName)
+    .subscribe(dataFromAPI => {
+      this.apilinggaIslandPhotoResult = dataFromAPI.apiLandingResult[0];
+      this.linggaIslandPhotoUrl = this.apilinggaIslandPhotoResult[1][3];
+
+      // need to run loop here if decide to set an interval and change cover photo every few seconds
+
+      // localStorage.setItem('sixtySecondsCategoryPhotos', JSON.stringify(this.apiCoverPhotoResult));
+
+      console.log(this.linggaIslandPhotoUrl);
+      console.log('lingga photo api call success');
+      console.log(this.apilinggaIslandPhotoResult);
+    });
   }
 
 }
+
