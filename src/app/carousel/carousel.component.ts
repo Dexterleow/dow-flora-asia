991d96a52e09e1c2667aecb7e28f3409fd9f85d2
dexterleow <gdleow@gmail.com>
2017-05-21 18:06:01 +0800

    Stage changes

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [{ provide: CarouselConfig, useValue: { interval: 15000, noPause: false } }]
 })
 export class CarouselComponent implements OnInit {
 
