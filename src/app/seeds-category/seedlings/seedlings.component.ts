bb84176d347642668b2fb39983266e35b9f4e378
dexterleow <gdleow@gmail.com>
2017-05-29 22:30:27 +0800

    Added Seeds pictures

--- src/app/seeds-category/seedlings/seedlings.component.ts
+++ src/app/seeds-category/seedlings/seedlings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GoogleSheetsService } from '../../shared/services/google-sheets.service';
+
 @Component({
   selector: 'app-seedlings',
   templateUrl: './seedlings.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeedlingsComponent implements OnInit {
 
-  constructor() { }
+  seedlingsPhotoUrl_1: string;
 
-  ngOnInit() {
+  seedsPhotoEndPoint: string;
+  seedsPhotoResult: Array<string>;
+
+  constructor(private googleSheetsService: GoogleSheetsService) {
   }
 
+  ngOnInit() {
+    this.seedsPhotoEndPoint = '/home/seeds';
+    this.seedsPhotoResult = [];
+
+    this.getImagesFromSheets(this.seedsPhotoEndPoint);
+  }
+  getImagesFromSheets(sheetName) {
+    this.googleSheetsService.getImages(sheetName)
+      .subscribe(dataFromAPI => {
+        this.seedsPhotoResult = dataFromAPI.apiLandingResult[0];
+        this.seedlingsPhotoUrl_1 = this.seedsPhotoResult[1][2];
+
+        console.log(this.seedlingsPhotoUrl_1);
+        console.log('seedlings photo api call success');
+        console.log(this.seedsPhotoResult);
+      });
+  }
 }
--- src/app/seeds-category/seeds/seeds.component.ts
+++ src/app/seeds-category/seeds/seeds.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GoogleSheetsService } from '../../shared/services/google-sheets.service';
+
 @Component({
   selector: 'app-seeds',
   templateUrl: './seeds.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeedsComponent implements OnInit {
 
-  constructor() { }
+  seedsPhotoUrl_1: string;
+
+  seedsPhotoEndPoint: string;
+  seedsPhotoResult: Array<string>;
+
+  constructor(private googleSheetsService: GoogleSheetsService) {
+  }
 
   ngOnInit() {
+    this.seedsPhotoEndPoint = '/home/seeds';
+    this.seedsPhotoResult = [];
+
+    this.getImagesFromSheets(this.seedsPhotoEndPoint);
+  }
+  getImagesFromSheets(sheetName) {
+    this.googleSheetsService.getImages(sheetName)
+      .subscribe(dataFromAPI => {
+        this.seedsPhotoResult = dataFromAPI.apiLandingResult[0];
+        this.seedsPhotoUrl_1 = this.seedsPhotoResult[0][2];
+
+        console.log(this.seedsPhotoUrl_1);
+        console.log('seeds photo api call success');
+        console.log(this.seedsPhotoResult);
+      });
   }
 
 }
