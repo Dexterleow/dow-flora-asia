0616b3fa13ed3ca68e938cb3aabd2335a8513cc0
dexterleow <gdleow@gmail.com>
2017-06-04 22:16:12 +0800

    Aligned the photos to bottom using flex in completed projects

--- src/app/operation-capability/operation-capability.component.ts
+++ src/app/operation-capability/operation-capability.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-operation-capability',
   templateUrl: './operation-capability.component.html',
-  styleUrls: ['./operation-capability.component.css']
+  styleUrls: ['./operation-capability.component.scss']
 })
 export class OperationCapabilityComponent implements OnInit {
 
