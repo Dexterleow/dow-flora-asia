3424e48b9a86e8db81c6123c7a37a67cece4ccf6
dexterleow <gdleow@gmail.com>
2017-07-03 22:35:57 +0800

    Changed email from Eugene to Dow Flora Darren

--- controllers/send-email.js
+++ controllers/send-email.js
@@ -16,7 +16,7 @@ router.post('/', (req, res) => {
   let mailOptions = {
     from: '"No Reply Dow Flora" <noreplydowflora@gmail.com>', // sender address
     // change to email eventually
-    to: 'eugeneoei89@gmail.com', // list of receivers
+    to: 'darren.dowflora@gmail.com', // list of receivers
     subject: 'Enquiry from ' + req.body.email, // Subject line
     html: `Hi Dow Flora,<br><br>You have an enquiry from ${req.body.name}.<br><br>
     His/Her enquiry is as follows:<br><br>
