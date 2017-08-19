ef5be3884245b4b2b579c1dc4500dbeac765f4dc
dexterleow <gdleow@gmail.com>
2017-08-20 02:23:11 +0800

    Load stone-table and pebble in the same function call to rectify race condition

--- src/app/stones/stone-table/stone-table.component.ts
+++ src/app/stones/stone-table/stone-table.component.ts
@@ -13,6 +13,8 @@ export class StoneTableComponent implements OnInit {
   plantsPhotoResult: Array<string>;
   stoneArrayLength: number;
 
+  pebblePhotoUrl_1: string;
+
   constructor(private googleSheetsService: GoogleSheetsService) {
   }
 
@@ -38,6 +40,9 @@ export class StoneTableComponent implements OnInit {
         this.plantsPhotoResult = dataFromAPI.apiLandingResult[0];
         this.stoneArrayLength = this.plantsPhotoResult.length;
         console.log(this.plantsPhotoResult);
+
+        // pebble url from GoogleSheet
+        this.pebblePhotoUrl_1 = this.plantsPhotoResult[6][2];
         localStorage.setItem('stones', JSON.stringify(this.plantsPhotoResult));
       });
   }
