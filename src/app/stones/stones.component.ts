64090f8c1f9f94bd85704cb9e058ab91f95b9471
dexterleow <gdleow@gmail.com>
2017-08-20 01:57:41 +0800

    Attempt to rectify stone page

--- src/app/stones/stones.component.ts
+++ src/app/stones/stones.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GoogleSheetsService } from '../shared/services/google-sheets.service';
+
 @Component({
   selector: 'app-stones',
   templateUrl: './stones.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StonesComponent implements OnInit {
 
-  constructor() { }
+  pebblePhotoUrl_1: string;
+
+  plantsPhotoEndPoint: string;
+  plantsPhotoResult: Array<string>;
+
+  constructor(private googleSheetsService: GoogleSheetsService) {
+  }
 
   ngOnInit() {
+    this.plantsPhotoEndPoint = '/home/pebble';
+    this.plantsPhotoResult = [];
+
+    this.getImagesFromSheets(this.plantsPhotoEndPoint);
+  }
+  getImagesFromSheets(sheetName) {
+    this.googleSheetsService.getImages(sheetName)
+      .subscribe(dataFromAPI => {
+        this.plantsPhotoResult = dataFromAPI.apiLandingResult[0];
+        this.pebblePhotoUrl_1 = this.plantsPhotoResult[0][2];
+
+        console.log(this.pebblePhotoUrl_1);
+        console.log('pebble photo api call success');
+        console.log(this.plantsPhotoResult);
+      });
   }
 
 }
